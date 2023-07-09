9c392a2ef0bdfb2eab0ad76a23809627590040b0
kazuemon <git@k6n.jp>
2023-07-10 03:03:35 +0900

    add: init tailwind, storybook

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/components/**/*.{ts,tsx}", "./src/stories/**/*.{ts,tsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
